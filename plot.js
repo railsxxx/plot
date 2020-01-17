b1e9107611eed812fa499ad98b7f7b23f9675367
railsxxx <rails@gmx-topmail.de>
2020-01-17 18:18:26 +0000

    implement simple strategy using objects

--- plot.js
+++ plot.js
@@ -1,18 +1,24 @@
 
 //https://plot.ly/javascript/plotlyjs-function-reference/#plotlyextendtraces
 
-// const md = require("./model.js");
-// const rand = md.rand;
+// plot ###################################
+function initPlot(graph, quotes, trades) {
+  let quotesIndex = [...Array(quotes.length).keys()];
+  let buy = {};
+  buy.x = trades.Buy.map((trade) => [trade.open.x, trade.close.x, null]);
+  buy.x = [].concat.apply([], buy.x);
+  buy.y = trades.Buy.map((trade) => [trade.open.y, trade.close.y, null]);
+  buy.y = [].concat.apply([], buy.y);
+  let sell = {};
+  sell.x = trades.Sell.map((trade) => [trade.open.x, trade.close.x, null]);
+  sell.x = [].concat.apply([], sell.x);
+  sell.y = trades.Sell.map((trade) => [trade.open.y, trade.close.y, null]);
+  sell.y = [].concat.apply([], sell.y);
+  let balance = {};
+  balance.x = trades.balance.x;
+  balance.y = trades.balance.y;
 
-// Plot ###########################################
-let quotes = [];
-let quotesIndex = [];
-let buy = { x: [], y: [] };
-let sell = { x: [], y: [] };
-let balance = { x: [], y: [] };
-
-function initPlot(graph) {
-  var trace1 = {
+  let trace1 = {
     name: 'quotes',
     x: quotesIndex,
     y: quotes,
@@ -23,7 +29,7 @@ function initPlot(graph) {
     line: { width: 1 },
     type: 'scatter'
   };
-  var trace2 = {
+  let trace2 = {
     name: 'buy',
     x: buy.x,
     y: buy.y,
@@ -38,7 +44,7 @@ function initPlot(graph) {
     connectgaps: false,
     type: 'scatter'
   };
-  var trace3 = {
+  let trace3 = {
     name: 'sell',
     x: sell.x,
     y: sell.y,
@@ -53,7 +59,7 @@ function initPlot(graph) {
     connectgaps: false,
     type: 'scatter'
   };
-  var trace4 = {
+  let balance1 = {
     name: 'balance',
     x: balance.x,
     y: balance.y,
@@ -68,9 +74,9 @@ function initPlot(graph) {
     type: 'scatter'
   };
 
-  var data = [trace1, trace2, trace3, trace4];
+  let data = [trace1, trace2, trace3, balance1];
 
-  var layout = {
+  let layout = {
     title: 'Quotes',
     xaxis: {
       rangeslider: {
@@ -90,25 +96,12 @@ function initPlot(graph) {
   };
 
   Plotly.newPlot(graph, data, layout);
-};
-
-// function extendPlot(graph) {
-//   let cnt = 0;
-//   let interval = setInterval(function() {
-//     Plotly.extendTraces(graph, {
-//       y: [[rand()], [rand()]]
-//     }, [0, 1])
-
-//     cnt = cnt + 1;
-//     if (cnt === 10) clearInterval(interval);
-//   }, 1000);
-// }
+}
 
 // Quotes ###########################################
-
 function rand() {
   return Math.random();
-};
+}
 function quotesGenerator() {
   let quotesCount = 100;
   quotes = [0];
@@ -117,100 +110,95 @@ function quotesGenerator() {
     quote += rand() < 0.5 ? -1 : 1;
     quotes.push(quote);
   }
-  quotesIndex = [...Array(quotesCount).keys()];
-};
+  return quotes;
+}
+
+// Balance ###########################################
+function Balance() {
+  this.total = 0;
+  this.x = [];
+  this.y = [];
+
+  this.update = function(trade) {
+    if (trade.type == 'buy') {
+      this.total += trade.close.y - trade.open.y;
+    }
+    if (trade.type == 'sell') {
+      this.total += trade.open.y - trade.close.y;
+    }
+    this.x.push(trade.close.x);
+    this.y.push(this.total);
+  }
+}
+
 // Trades ###########################################
 function Point(x, y) {
   this.x = x;
   this.y = y;
-};
+}
 function Trade(type, open) {
   this.type = type;
   this.open = open;
   this.close;
-};
-let tradesBuy = [];
-let tradesSell = [];
+}
+function Trades() {
+  this.Buy = [];
+  this.Sell = [];
+  this.balance = new Balance();
+}
+Trades.prototype.buy = function(index, quote) {
+  let point = new Point(index, quote);
+  let trade = new Trade('buy', point);
+  this.Buy.push(trade);
+  return trade;
+}
+Trades.prototype.sell = function(index, quote) {
+  let trade = new Trade('sell', new Point(index, quote));
+  this.Sell.push(trade);
+  return trade;
+}
+Trades.prototype.close = function(trade, index, quote) {
+  trade.close = new Point(index, quote);
+  this.balance.update(trade);
+}
 
-function tradeGenerator() {
-  initBalance();
-  buy.x.length = buy.y.length = 0;
-  sell.x.length = sell.y.length = 0;
-  let trade;
-  for (let i = 1; i < quotes.length; i++) {
+// Strategy ###########################################
+function strategySimple(quotes) {
+  trades = new Trades();
+  let trade = undefined;
+  let i;
+  for (i = 1; i < quotes.length; i++) {
     if (quotes[i] > quotes[i - 1]) {
       // buy
       if (trade && trade.type == 'sell') {
-        trade.close = new Point(i, quotes[i]);
-        sell.x.push(trade.open.x);
-        sell.y.push(trade.open.y);
-        sell.x.push(trade.close.x);
-        sell.y.push(trade.close.y);
-        sell.x.push(null);
-        sell.y.push(null);
-        Balance(trade);
+        trades.close(trade, i, quotes[i]);
         trade = undefined;
       }
       if (!trade) {
-        trade = new Trade('buy', new Point(i, quotes[i]));
-        tradesBuy.push(trade);
+        trade = trades.buy(i, quotes[i]);
       }
     }
     if (quotes[i] < quotes[i - 1]) {
       // sell
       if (trade && trade.type == "buy") {
-        trade.close = new Point(i, quotes[i]);
-        buy.x.push(trade.open.x);
-        buy.y.push(trade.open.y);
-        buy.x.push(trade.close.x);
-        buy.y.push(trade.close.y);
-        buy.x.push(null);
-        buy.y.push(null);
-        Balance(trade);
+        trades.close(trade, i, quotes[i]);
         trade = undefined;
       }
       if (!trade) {
-        trade = new Trade('sell', new Point(i, quotes[i]));
-        tradesSell.push(trade);
+        trade = trades.sell(i, quotes[i]);
       }
     }
   }
-}
-// Balance ###########################################
-let balanceTotal = 0;
-
-function initBalance(){
-  balanceTotal = 0;
-  balance.x.length = balance.y.length = 0;
-  balance.x.push(0);
-  balance.y.push(0);
+  // close last trade at end of quotes
+  trades.close(trade, i - 1, quotes[i - 1]);
+  return trades;
 }
 
-function Balance(trade) {
-  if (trade.type == 'buy') {
-    balanceTotal += trade.close.y - trade.open.y;
-    let id = tradesBuy.indexOf(trade);
-    tradesBuy.splice(id, 1);
-  }
-  if (trade.type == 'sell') {
-    balanceTotal += trade.open.y - trade.close.y;
-    let id = tradesSell.indexOf(trade);
-    tradesSell.splice(id, 1);
-  }
-  balance.x.push(trade.close.x);
-  balance.y.push(balanceTotal);
-  //console.log(balance);
-}
-
-function showBalance() {
-  console.log("Balance: " + balanceTotal);
-}
-// Run ###########################################
+// Run  ###########################################
 function showPlot(el) {
-  quotesGenerator();
-  tradeGenerator();
-  initPlot(el);
-  //extendPlot(el);
-  showBalance()
+  let quotes = quotesGenerator();
+  let tradesSimple = strategySimple(quotes);
+  initPlot(el, quotes, tradesSimple);
 }
+
 
