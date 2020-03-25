edbf199b2a2f3877546713d1e8ec3b5d103c39b6
railsxxx <rails@gmx-topmail.de>
2020-03-25 16:49:56 +0000

    save stack

--- strategies.js
+++ strategies.js
@@ -377,3 +377,265 @@
   //   trades.updateEquity(quotes[i - 1]);
   //   return trades;
   // }
+  
+  //   function strategySimple3_2(quotes) {
+  //   pairs = new Pairs();
+  //   trades = new Trades();
+  //   tradeID = 0;
+  //   let tradeCountMaxOpen = 40;
+  //   let tradeCountOpen = 0;
+  //   let trade = opTrade = prof1Trade = tradeSell = tradeBuy = undefined;
+  //   let pairTrade = undefined;
+  //   let hasPair = false;
+  //   let pair = undefined;
+  //   let minProfit = 0;
+  //   let i = 0;
+  //   for (i = 1; i < quotes.length; i++) {
+  //     console.log("index: " + i);
+  //     // console.log(trade);
+  //     trades.updateTradesOpenFullRange();
+  //     // ** check tradeCountMaxOpen
+  //     // tradeCountOpen = trades.tradeCountAllOpen - tradeCountMaxOpen;
+  //     // console.log("tradeCountOpen: " + tradeCountOpen);
+  //     // if (tradeCountOpen >= 0) return trades;
+  //     // while (tradeCountOpen > 0) {
+  //     //   // tradeSell = trades.SellHighest;
+  //     //   // tradeBuy = trades.BuyLowest;
+  //     //   tradeSell = trades.SellLowest;
+  //     //   tradeBuy = trades.BuyHighest;
+  //     //   trades.close(tradeSell, i, quotes[i]);
+  //     //   trades.close(tradeBuy, i, quotes[i]);
+  //     //   console.log("trades closed: profit: " + (tradeSell.getProfit() + tradeBuy.getProfit()));
+  //     //   console.log(tradeSell);
+  //     //   console.log(tradeBuy);
+  //     //   // reinit SellHighest, BuyLowest
+  //     //   trades.updateTradesOpenFullRange();
+  //     //   tradeCountOpen -= 2;
+  //     // }
+  //     // ** end check tradeCountMaxOpen
+  //     trades.updateEquity(quotes[i]);
+
+  //     // buy
+  //     if (quotes[i] > quotes[i - 1]) {
+  //       // create and register pair
+  //       if (trade && trade.type == 'sell' && !trade.pair) {
+  //         // if (pair && pair.sell.open.y >= quotes[i]) {
+  //         //   pairs.close(pair, i, quotes[i]);
+  //         // }
+  //         // pairTrade = trades.buy(i, quotes[i]);
+  //         // pair = pairs.add(pairTrade, trade);
+  //         trades.close(trade, i, quotes[i]);
+  //         hasPair = true;
+  //         if ((trades.equity[i] + trades.balance.total) > minProfit) {
+  //           trades.closeAllOpenTrades(i, quotes[i]);
+  //           trades.balance.total = 0;
+  //         }
+  //       }
+  //       // if (pair) hasPair = true;
+
+  //       // trade = pairTrade;
+  //       trade = undefined;
+  //       // if (!hasPair || (hasPair && pair.buy.open.y < quotes[i])) {
+  //       if (!hasPair) {
+  //         // close no pair opposite trade at same quote
+  //         opTrade = trades.isOpenSellNoPairAt(i, quotes[i]);
+  //         if (opTrade)
+  //           trades.close(opTrade, i, quotes[i]);
+  //         // create buy trade if no pairTrade and no opposite trade
+  //         // if (!opTrade) {
+  //         else {
+  //           trade = trades.buy(i, quotes[i]);
+  //         }
+  //       }
+  //     }
+  //     // sell  
+  //     if (quotes[i] < quotes[i - 1]) {
+  //       // create and register pair
+  //       if (trade && trade.type == 'buy' && !trade.pair) {
+  //         // if (pair && pair.buy.open.y <= quotes[i]) {
+  //         //   pairs.close(pair, i, quotes[i]);
+  //         // }
+  //         // pairTrade = trades.sell(i, quotes[i]);
+  //         // pair = pairs.add(trade, pairTrade);
+  //         trades.close(trade, i, quotes[i]);
+  //         hasPair = true;
+  //         if ((trades.equity[i] + trades.balance.total) > minProfit) {
+  //           trades.closeAllOpenTrades(i, quotes[i]);
+  //           trades.balance.total = 0;
+  //         }
+  //       }
+  //       // if (pair) hasPair = true;
+
+  //       // trade = pairTrade;
+  //       trade = undefined;
+  //       // if (!hasPair || (pair.sell.open.y > quotes[i])) {
+  //       if (!hasPair) {
+  //         // close no pair opposite trade at same quote
+  //         opTrade = trades.isOpenBuyNoPairAt(i, quotes[i]);
+  //         if (opTrade)
+  //           trades.close(opTrade, i, quotes[i]);
+  //         // create sell trade if no pairTrade and no opposite trade
+  //         // if (!opTrade) {
+  //         else {
+  //           trade = trades.sell(i, quotes[i]);
+  //         }
+  //       }
+  //     }
+  //     // console.log(pairTrade);
+  //     pairTrade = undefined;
+  //     hasPair = false;
+  //   }
+  //   // close all open trades at end of quotes
+  //   // trades.closeAllOpenBuyNoPair(i - 1, quotes[i - 1]);
+  //   // trades.closeAllOpenSellNoPair(i - 1, quotes[i - 1]);
+  //   // trades.closeAllOpenBuy(i - 1, quotes[i - 1]);
+  //   // trades.closeAllOpenSell(i - 1, quotes[i - 1]);
+  //   trades.closeAllOpenTrades(i - 1, quotes[i - 1]);
+  //   trades.updateEquity(quotes[i - 1]);
+  //   return trades;
+  // }
+  // function strategySimple3_3(quotes) {
+  //   pairs = new Pairs();
+  //   trades = new Trades();
+  //   tradeID = 0;
+  //   let tradeCountMaxOpen = 40;
+  //   let tradeCountOpen = 0;
+  //   let trade = opTrade = prof1Trade = tradeSell = tradeBuy = undefined;
+  //   let pairTrade = undefined;
+  //   let hasPair = false;
+  //   let pair = undefined;
+  //   let direction = undefined;
+  //   let minProfit = 0;
+  //   let i = 0;
+  //   for (i = 1; i < quotes.length; i++) {
+  //     console.log("index: " + i);
+  //     // console.log(trade);
+  //     trades.updateTradesOpenFullRange();
+  //     // ** check tradeCountMaxOpen
+  //     // tradeCountOpen = trades.tradeCountAllOpen - tradeCountMaxOpen;
+  //     // console.log("tradeCountOpen: " + tradeCountOpen);
+  //     // if (tradeCountOpen >= 0) return trades;
+  //     // while (tradeCountOpen > 0) {
+  //     //   // tradeSell = trades.SellHighest;
+  //     //   // tradeBuy = trades.BuyLowest;
+  //     //   tradeSell = trades.SellLowest;
+  //     //   tradeBuy = trades.BuyHighest;
+  //     //   trades.close(tradeSell, i, quotes[i]);
+  //     //   trades.close(tradeBuy, i, quotes[i]);
+  //     //   console.log("trades closed: profit: " + (tradeSell.getProfit() + tradeBuy.getProfit()));
+  //     //   console.log(tradeSell);
+  //     //   console.log(tradeBuy);
+  //     //   // reinit SellHighest, BuyLowest
+  //     //   trades.updateTradesOpenFullRange();
+  //     //   tradeCountOpen -= 2;
+  //     // }
+  //     // ** end check tradeCountMaxOpen
+  //     trades.updateEquity(quotes[i]);
+
+  //     // buy
+  //     if (quotes[i] > quotes[i - 1]) {
+  //       if (direction == "sell") {
+  //         // check exit condition
+  //         if ((trades.equity[i] + trades.balance.total) > minProfit) {
+  //           trades.closeAllOpenTrades(i, quotes[i]);
+  //           trades.balance.total = 0;
+  //           trade = undefined;
+  //           pair = undefined;
+  //         }
+  //         // create and update pair
+  //         if (trade && trade.type == 'sell' && !trade.pair) {
+  //           if (pair) {
+  //             if (pair.sell)
+  //               trades.close(pair.sell, i, quotes[i]);
+  //             pair.sell = trade;
+  //             trade.pair = pair;
+  //           } else {
+  //             pairTrade = trades.buy(i, quotes[i]);
+  //             pair = pairs.add(pairTrade, trade);
+  //           }
+  //         } else {
+  //           if (pair) {
+  //             if (pair.sell)
+  //               trades.close(pair.sell, i, quotes[i]);
+  //             pair.sell = undefined;
+  //           }
+  //         }
+  //         trade = undefined;
+  //         // check pair buy
+  //         if (pair && !pair.buy) {
+  //           pair.buy = trades.buy(i, quotes[i]);
+  //           pair.buy.pair = pair;
+  //         }
+  //       } else {
+  //         // close no pair opposite trade at same quote
+  //         opTrade = trades.isOpenSellNoPairAt(i, quotes[i]);
+  //         if (opTrade)
+  //           trades.close(opTrade, i, quotes[i]);
+  //         // create buy trade if no pairTrade and no opposite trade
+  //         // if (!opTrade) {
+  //         else {
+  //           trade = trades.buy(i, quotes[i]);
+  //         }
+  //       }
+  //       direction = "buy";
+  //     }
+  //     // sell  
+  //     if (quotes[i] < quotes[i - 1]) {
+  //       if (direction == "buy") {
+  //         // check exit condition
+  //         if ((trades.equity[i] + trades.balance.total) > minProfit) {
+  //           trades.closeAllOpenTrades(i, quotes[i]);
+  //           trades.balance.total = 0;
+  //           trade = undefined;
+  //           pair = undefined;
+  //         }
+  //         // create and register pair
+  //         if (trade && trade.type == 'buy' && !trade.pair) {
+  //           if (pair) {
+  //             if (pair.buy)
+  //               trades.close(pair.buy, i, quotes[i]);
+  //             pair.buy = trade;
+  //             trade.pair = pair;
+  //           } else {
+  //             pairTrade = trades.sell(i, quotes[i]);
+  //             pair = pairs.add(pairTrade, trade);
+  //           }
+  //         } else {
+  //           if (pair) {
+  //             if (pair.buy)
+  //               trades.close(pair.buy, i, quotes[i]);
+  //             pair.buy = undefined;
+  //           }
+  //         }
+  //         trade = undefined;
+  //         // check pair sell
+  //         if (pair && !pair.sell) {
+  //           pair.sell = trades.sell(i, quotes[i]);
+  //           pair.sell.pair = pair;
+  //         }
+  //       } else {
+  //         // close no pair opposite trade at same quote
+  //         opTrade = trades.isOpenBuyNoPairAt(i, quotes[i]);
+  //         if (opTrade)
+  //           trades.close(opTrade, i, quotes[i]);
+  //         // create sell trade if no pairTrade and no opposite trade
+  //         // if (!opTrade) {
+  //         else {
+  //           trade = trades.sell(i, quotes[i]);
+  //         }
+  //       }
+  //       direction = "sell";
+  //     }
+  //     // console.log(pairTrade);
+  //     pairTrade = undefined;
+  //     hasPair = false;
+  //   }
+  //   // close all open trades at end of quotes
+  //   // trades.closeAllOpenBuyNoPair(i - 1, quotes[i - 1]);
+  //   // trades.closeAllOpenSellNoPair(i - 1, quotes[i - 1]);
+  //   // trades.closeAllOpenBuy(i - 1, quotes[i - 1]);
+  //   // trades.closeAllOpenSell(i - 1, quotes[i - 1]);
+  //   trades.closeAllOpenTrades(i - 1, quotes[i - 1]);
+  //   trades.updateEquity(quotes[i - 1]);
+  //   return trades;
+  // }
